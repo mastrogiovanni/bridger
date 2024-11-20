eb871adb020966a90fe891e7825121907a19ea94
Michele Mastrogiovanni <michele.mastrogiovanni@elerian.ai>
2024-11-21 00:37:18 +0100

    doc: script

--- start-docker.sh
+++ start-docker.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+IMAGE=mastrogiovanni/bridger:v0.0.1
+
 docker run -it \
     -v $(pwd)/config.yaml:/app/config.yaml \
     -v /home/michele/.ssh/known_hosts:/root/.ssh/known_hosts \
     -v /home/michele/.ssh/id_rsa:/root/.ssh/id_rsa \
     -p 2345:2345 \
+    -p 2346:2346 \
     -p 1234:1234 \
-    mastrogiovanni/bridger:main bridger \
+    $IMAGE bridger \
         /app/config.yaml \
-        uat:calm:2345 \
+        local:whoami:2345 \
+        uat:calm:2346 \
         uat:db:1234
