7a20b504432011dcfa202aedfc87277cd8c0aacf
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2024-11-21 20:05:46 +0100

    Update start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -4,13 +4,9 @@ IMAGE=mastrogiovanni/bridger:v0.0.1
 
 docker run -it \
     -v $(pwd)/config.yaml:/app/config.yaml \
-    -v /home/michele/.ssh/known_hosts:/root/.ssh/known_hosts \
-    -v /home/michele/.ssh/id_rsa:/root/.ssh/id_rsa \
-    -p 2345:2345 \
-    -p 2346:2346 \
-    -p 1234:1234 \
+    -v /home/user/.ssh/known_hosts:/root/.ssh/known_hosts \
+    -v /home/user/.ssh/id_rsa:/root/.ssh/id_rsa \
+    -p 9000:9000 \
     $IMAGE bridger \
         /app/config.yaml \
-        local:whoami:2345 \
-        uat:calm:2346 \
-        uat:db:1234
+        dev:api-service:9000
